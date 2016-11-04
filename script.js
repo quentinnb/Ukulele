610ed7c777c9739359992adff580f0d4876c2ed9
quentinnb <Quentin@MacBook-Air-de-Quentin.local>
2016-11-04 13:59:58 +0100

    Check Yo

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 $(document).ready(function() {
 
 var videos = [
-{'url':'HQ8Avkxv3-4', 'name':'Don\'t worry, be happy', 'artist':'Bobby McFerrin', 'level':'test'},
-{'url':'nq34lN39sj8', 'name':'Raggamuffin', 'artist':'Selah Sue', 'level':'test1'},
-{'url':'pRpvdcjkT3k', 'name':'lolo', 'artist':'Bobby test', 'level':'test2'},
+{'url':'HQ8Avkxv3-4', 'name':'Don\'t worry, be happy', 'artist':'Bobby McFerrin', 'level':'test', 'chords':'http://www.ukulele-tabs.com/fr/tablatures/bobby-mcferrin/dont-worry-be-happy-uke-tab-18613.html', 'tutorial':'https://www.youtube.com/watch?v=4sQBqFp5uFo'},
+{'url':'nq34lN39sj8', 'name':'Raggamuffin', 'artist':'Selah Sue', 'level':'test1', 'chords':'http://www.ukulele-tabs.com/fr/tablatures/bobby-mcferrin/dont-worry-be-happy-uke-tab-18613.html','tutorial':'http://digibidule.fr'},
+{'url':'pRpvdcjkT3k', 'name':'lolo', 'artist':'Bobby test', 'level':'test2', 'chords':'http://www.ukulele-tabs.com/fr/tablatures/bobby-mcferrin/dont-worry-be-happy-uke-tab-18613.html', 'tutorial':'http://digibidule.fr'},
 ];
 
 var index=Math.floor(Math.random() * videos.length);
@@ -20,6 +20,9 @@ $("#video-artist").html(videoOptions.artist);
 
 $("#video-level").html(videoOptions.level);
 
+$("#chords-link").attr('href', videoOptions.chords);
+
+$("#tuto-link").attr('href', videoOptions.tutorial);
 
 });
 
